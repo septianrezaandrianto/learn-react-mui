44a63e19c11569b14785a9ab8f7145949ec85a94
Septian Reza <septian.reza@infinitium.com>
2024-10-11 18:49:07 +0700

    implemented dynamic .env file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { Typography } from "@mui/material";
+import { env } from "./env";
 import TypographyLearn from "./TypographyLearn";
 
 function App() {
   return (
+    <>
     <div>
-      <TypographyLearn/>
+      <Typography
+          variant='h1' 
+          sx={{
+            textAlign : 'center',
+            color : 'black',
+          }}
+      >
+        Hello World from {env.REACT_APP_ENV}
+      </Typography>
+      <Typography  sx={{
+          textAlign : 'center',
+          color : 'red',
+          fontSize : '24px'
+        }}
+      >
+        Versi {env.REACT_APP_VERSION}
+      </Typography >
     </div>
+    {/* <div>
+      <TypographyLearn/>
+    </div> */}
+    </>
   );
 }
 
